c1dc0a44f2e6dc48428cad7c70fb6292a05618c1
Mithul Rao <mithul@sparkt.in>
2018-10-04 12:08:41 +0530

    UI Fixes

--- Sparkt.WebApp/Global.asax.cs
+++ Sparkt.WebApp/Global.asax.cs
@@ -18,6 +18,7 @@ namespace Sparkt.WebApp
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
             BootStrap.Build();
+            MvcHandler.DisableMvcResponseHeader = true;
         }
     }
 }
